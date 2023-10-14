969fa181fab6fb2949b1bca91f83fe3d83ee9c26
Sergey <125288167+Ser9330026@users.noreply.github.com>
2023-10-14 13:12:32 +0500

    Добавьте файлы проекта.

---
+++ Работа с файлами ifstream/Работа с файлами ifstream.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	std::string path = "test.txt";
+	// Для считывания используем класс ifstream
+	std::ifstream fin;
+	fin.open(path);
+	if (!fin.is_open()) {
+		std::cout << "Файл не открылся" << std::endl;
+	}
+	else {
+		std::cout << "Файл открылся" << std::endl;
+		//char ch;// Посимольное считывание из файла
+		//while (fin.get(ch)) {
+		//	std::cout << ch;
+		//}
+
+		std::string str;// Построковое считывание файлов
+		while (!fin.eof()) { // eof() отлавливает конец файла
+			//fin >> str; // каждое слов на новой строке
+			std::getline(fin, str); //строки считываются целиком
+			std::cout << str << std::endl;
+		}
+
+	}
+
+	fin.close();
+
+
+
+
+	return 0;
+}
